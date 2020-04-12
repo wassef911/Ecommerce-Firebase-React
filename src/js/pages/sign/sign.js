97e199d15460922d11b4ebcdc8b475084e1eca41
wassef911 <1.wassef911@gmail.com>
2020-04-12 15:00:25 +0100

    fixed responsive behavior

--- src/js/pages/sign/sign.js
+++ src/js/pages/sign/sign.js
@@ -4,7 +4,7 @@ import SignIn from "../../containers/signin";
 import SignUp from "../../containers/signup";
 function Sign() {
   return (
-    <div className="sign d-flex flex-wrap justify-content-around pt-4">
+    <div className="sign d-flex flex-sm-column flex-xl-row flex-wrap align-items-center justify-content-around pt-4">
       <SignIn />
       <br />
       <SignUp />
