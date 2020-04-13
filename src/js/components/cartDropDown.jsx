da93126af98f506724888f8dc121cf24846b73cc
wassef911 <1.wassef911@gmail.com>
2020-04-13 14:53:27 +0100

    added shopcart

---
+++ src/js/components/cartDropDown.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+function CartDropDown() {
+  return (
+    <div
+      class="modal fade"
+      id="exampleModalScrollable"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalScrollableTitle"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-scrollable" role="document">
+        <div class="modal-content">
+          <button
+            type="button"
+            class="close btn btn-primary"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+
+          <div class="modal-body">...</div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-outline-primary btn-sm btn-block"
+            >
+              GO TO CHECKOUT
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CartDropDown;
--- src/js/components/navbar.js
+++ src/js/components/navbar.js
@@ -6,63 +6,72 @@ import { connect } from "react-redux";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import "./navbar.scss";
 import CartIcon from "./cart-icon";
+import CartDropDown from "./cartDropDown";
 
 const NavBar = ({ currentUser }) => {
   return (
-    <nav
-      className="mb-1 navbar navbar-expand-lg navbar-light bg-light "
-      style={{
-        clipPath: " polygon( 0 0, 100% 0, 100% 100%, 0 calc(96% - 1vw) )",
-      }}
-    >
-      <a className="navbar-brand" href="/">
-        <AiOutlineShoppingCart
-          style={{ color: "#ffa45c", marginLeft: "2vm" }}
-          size={45}
-        />
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent-4"
-        aria-controls="navbarSupportedContent-4"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+    <div>
+      <nav
+        className="mb-1 navbar navbar-expand-lg navbar-light bg-light "
+        style={{
+          clipPath: " polygon( 0 0, 100% 0, 100% 100%, 0 calc(96% - 1vw) )",
+        }}
       >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent-4">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="/shop">
-              SHOP
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/contact">
-              CONTACT
-            </a>
-          </li>
-          <li className="nav-item">
-            {currentUser ? (
-              <a className="nav-link" href="/" onClick={() => auth.signOut()}>
-                SIGN OUT
+        <a className="navbar-brand" href="/">
+          <AiOutlineShoppingCart
+            style={{ color: "#ffa45c", marginLeft: "2vm" }}
+            size={45}
+          />
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent-4"
+          aria-controls="navbarSupportedContent-4"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent-4">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="/shop">
+                SHOP
               </a>
-            ) : (
-              <Link className="nav-link" to="/sign">
-                SIGN IN
-              </Link>
-            )}
-          </li>
-          <li className="nav-item">
-            <a className="nav-link">
-              <CartIcon />
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/contact">
+                CONTACT
+              </a>
+            </li>
+            <li className="nav-item">
+              {currentUser ? (
+                <a className="nav-link" href="/" onClick={() => auth.signOut()}>
+                  SIGN OUT
+                </a>
+              ) : (
+                <Link className="nav-link" to="/sign">
+                  SIGN IN
+                </Link>
+              )}
+            </li>
+            <li className="nav-item">
+              <button
+                type="button"
+                class="btn nav-link"
+                data-toggle="modal"
+                data-target="#exampleModalScrollable"
+              >
+                <CartIcon />
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <CartDropDown />
+    </div>
   );
 };
 
