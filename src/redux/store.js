8044ef832eb2094c4fceb0a587513c551c349ee4
wassef911 <1.wassef911@gmail.com>
2020-04-30 04:07:40 +0100

    fixed bug

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,15 +3,6 @@ import rootReducer from "./root-reducer";
 import { persistStore } from "redux-persist";
 import thunk from "redux-thunk";
 
-let composeEnhancer = undefined;
-
-if (process.env.NODE_ENV === "development") {
-  composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-}
-
-export const store = createStore(
-  rootReducer,
-  composeEnhancer(applyMiddleware(thunk))
-);
+export const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export const persistor = persistStore(store);
