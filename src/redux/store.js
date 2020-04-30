2af6d28ce371f8643a2ab1622670d90eb131f96e
wassef911 <1.wassef911@gmail.com>
2020-04-30 04:17:04 +0100

    .

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,15 @@ import rootReducer from "./root-reducer";
 import { persistStore } from "redux-persist";
 import thunk from "redux-thunk";
 
-export const store = createStore(rootReducer, applyMiddleware(thunk));
+let composeEnhancer = undefined;
+
+if (process.env.NODE_ENV === "development") {
+  composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
+export const store = createStore(
+  rootReducer,
+  composeEnhancer(applyMiddleware(thunk))
+);
 
 export const persistor = persistStore(store);
