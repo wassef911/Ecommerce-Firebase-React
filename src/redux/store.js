1bb2416f2a889b518067279cc84322270d4e4416
wassef911 <1.wassef911@gmail.com>
2020-05-07 07:26:51 +0100

    added redux dev tool

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 import createSagaMiddleware from "redux-saga";
@@ -14,8 +15,10 @@ if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
-
+export const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(...middlewares))
+);
 sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
