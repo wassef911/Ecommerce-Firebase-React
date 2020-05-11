0e0e7bfdb8aa332dcf28f5adcd1da69de3797484
wassef911 <1.wassef911@gmail.com>
2020-05-11 01:28:49 +0100

    added backend

--- client/src/js/components/stripeButton.jsx
+++ client/src/js/components/stripeButton.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 function StripeButton({ price }) {
+  const PriceForStripe = price * 100;
   const publishableKey = "pk_test_tgNtACJqPaoo7ni1g4aQFYZC00L438N0ZF";
-  const onToken = () => {
-    alert("Payment Successsful");
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: PriceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        alert("Payment successful");
+      })
+      .catch((err) => {
+        console.log("Payment error :", JSON.parse(err));
+        alert(
+          " There was an issue with your payment. Please make sure you use the provided credit card "
+        );
+      });
   };
   return (
     <>
@@ -14,9 +32,9 @@ function StripeButton({ price }) {
         billingAddress
         shippingAddress
         description={`Your total is ${price} DT`}
-        amount={price * 100}
+        amount={PriceForStripe}
         panelLabel="Pay Now"
-        token={onToken}
+        token={() => onToken()}
         stripeKey={publishableKey}
         ComponentClass="div"
       />
