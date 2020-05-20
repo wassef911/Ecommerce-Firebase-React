bbdd8f50a5be8e7080a5329b6235d18b49ba358a
wassef911 <1.wassef911@gmail.com>
2020-05-20 06:55:22 +0100

    ...

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use(compression);
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(express.static(path.join(__dirname, "client/build")));
 
   app.get("*", function (req, res) {
